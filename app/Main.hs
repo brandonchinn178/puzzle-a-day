576ec2443f4fa54d6699aa033f358a55fb1f51c3
Brandon Chinn <brandonchinn178@gmail.com>
2023-01-08 13:02:20 -0800

    Use optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,72 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
 
+import Control.Monad (guard)
+import Data.Maybe (fromMaybe)
 import Data.Time (UTCTime (..), getCurrentTime, toGregorian)
-import System.Environment (getArgs)
+import Options.Applicative
 import Text.Read (readMaybe)
 import Text.Printf (printf)
 
 import SolverDFS qualified as DFS
 import Render (renderBoard, renderMonth)
 
+data CLIOptions = CLIOptions
+  { cliMonth :: Maybe Int
+  , cliDay :: Maybe Int
+  , cliAlgorithm :: Algorithm
+  }
+
+data Algorithm = DFS
+
+parseCLIOptions :: ParserInfo CLIOptions
+parseCLIOptions = info (options <**> helper) fullDesc
+  where
+    options =
+      CLIOptions
+        <$> argument (Just <$> maybeReader parseMonth)
+              ( metavar "MONTH"
+              <> help "Month (1-12)"
+              <> value Nothing
+              )
+        <*> argument (Just <$> maybeReader parseDay)
+              ( metavar "DAY"
+              <> help "Day (1-31)"
+              <> value Nothing
+              )
+        <*> option (maybeReader parseAlgorithm)
+              ( long "solver"
+              <> help "choices: dfs (default), genetic"
+              <> value DFS
+              )
+
+    parseMonth m = do
+      m' <- readMaybe m
+      guard (1 <= m' && m' <= 12)
+      pure m'
+
+    parseDay d = do
+      d' <- readMaybe d
+      guard (1 <= d' && d' <= 31)
+      pure d'
+
+    parseAlgorithm = \case
+      "dfs" -> Just DFS
+      _ -> Nothing
+
 main :: IO ()
 main = do
-  (month, day) <-
-    getArgs >>= \case
-      [] -> do
-        UTCTime today _ <- getCurrentTime
-        let (_, m, d) = toGregorian today
-        pure (m, d)
-      [m, d]
-        | Just m' <- readMaybe m
-        , Just d' <- readMaybe d
-        , 1 <= m', m' <= 12
-        , 1 <= d', d' <= 31 ->
-            pure (m', d')
-      _ -> error $ "Usage: ./puzzle-a-day [MONTH DAY]"
+  CLIOptions{..} <- execParser parseCLIOptions
 
-  printf "\n============= Solving for %s %d =============\n\n" (renderMonth month) day
+  UTCTime today _ <- getCurrentTime
+  let (_, currMonth, currDay) = toGregorian today
 
-  let solution = DFS.solve (month, day)
-  putStrLn (renderBoard solution)
+  let
+    month = fromMaybe currMonth cliMonth
+    day = fromMaybe currDay cliDay
+
+  case cliAlgorithm of
+    DFS -> do
+      printf "\n============= Solving for %s %d =============\n\n" (renderMonth month) day
+      let solution = DFS.solve (month, day)
+      putStrLn (renderBoard solution)
